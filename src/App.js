1ad20073ef16f4a803a71bb62190fe32209a4abe
Yannick Makwenge <yannickmakwenge@gmail.com>
2023-02-08 09:16:33 +0200

    fixing issue with the footer on the mobile version

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Words from './components/Words.js'
 import './styles/style.scss';
-
+import useWindowSize from './components/useWindowSize'
+import checkDevice from './components/checkDevice'
 
 function App() {
-  return (
-    <div className="App">
-      <Words />
-      <div className='footer py-3'>
-        <span >Created by <a href="https://github.com/Yannick-M243" target='blank'>Yannick Makwenge</a></span>
+  //Checking the type of device
+  const size = useWindowSize();
+  const isMobile = checkDevice(size);
+
+  //changing the footer's position property for mobiles
+  if (isMobile) {
+    return (
+      <div className="App">
+        <Words />
+        <div className='footer-mobile py-3'>
+          <span >Created by <a href="https://github.com/Yannick-M243" target='blank'>Yannick Makwenge</a></span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="App">
+        <Words />
+        <div className='footer-computer py-3'>
+          <span >Created by <a href="https://github.com/Yannick-M243" target='blank'>Yannick Makwenge</a></span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
